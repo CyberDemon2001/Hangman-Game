764a9a9c18065eb5f860675a2c55ac949b83349b
CyberDemon2001 <110963899+CyberDemon2001@users.noreply.github.com>
2024-07-25 14:43:55 +0530

    Update HangmanCanvas.js

--- src/HangmanCanvas.js
+++ src/HangmanCanvas.js
@@ -1,22 +1,73 @@
+
+
 import React from "react";
 
+const HangmanCanvas = ({ mistakes }) => {
+  const canvasWidth = 200;
+  const canvasHeight = 200;
+  const canvasRef = React.useRef(null);
 
-const HangmanCanvas=({mistakes})=>{
-    const parts=[
-        'head',
-        'body',
-        'left-arm',
-        'right-arm',
-        'left-leg',
-        'right-leg',
-    ];
-    return(
-        <div className="hangman-canvas">
-            {parts.slice(0,mistakes).map((part,index)=>(
-                <div key={index} className={part} />
-            ))}
-        </div>
-    );
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    if (mistakes >= 1) {
+      ctx.beginPath();
+      ctx.moveTo(20, canvasHeight - 20);
+      ctx.lineTo(canvasWidth - 20, canvasHeight - 20);
+      ctx.stroke();
+    }
+
+    if (mistakes >= 2) {
+      ctx.beginPath();
+      ctx.arc(canvasWidth / 2, 50, 30, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+
+    if (mistakes >= 2) {
+        ctx.beginPath();
+        ctx.moveTo(canvasWidth / 2, 80);
+        ctx.lineTo(canvasWidth / 2, 140);
+        ctx.stroke();
+      }
+  
+      if (mistakes >= 3) {
+        ctx.beginPath();
+        ctx.moveTo(canvasWidth / 2, 100);
+        ctx.lineTo(canvasWidth / 2 - 20, 120);
+        ctx.stroke();
+      }
+  
+      if (mistakes >= 4) {
+        ctx.beginPath();
+        ctx.moveTo(canvasWidth / 2, 100);
+        ctx.lineTo(canvasWidth / 2 + 20, 120);
+        ctx.stroke();
+      }
+  
+      if (mistakes >= 5) {
+        ctx.beginPath();
+        ctx.moveTo(canvasWidth / 2, 140);
+        ctx.lineTo(canvasWidth / 2 - 20, 160);
+        ctx.stroke();
+      }
+  
+    
+      if (mistakes >= 6) {
+        ctx.beginPath();
+        ctx.moveTo(canvasWidth / 2, 140);
+        ctx.lineTo(canvasWidth / 2 + 20, 160);
+        ctx.stroke();
+      }
+    }, [mistakes]);
+
+  return (
+    <div className="hangman-canvas">
+      <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />
+    </div>
+  );
 };
 
 export default HangmanCanvas;
